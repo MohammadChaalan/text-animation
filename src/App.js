709e43cc183eca42da995d4417b4f4b1292dc055
Your Name <you@example.com>
2023-03-21 17:56:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Animation from "./animation/Animation";
+function App() {
+  return (
+    <div className="App">
+     
+     <Animation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/animation/Animation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./animation.css"
+
+ function Animation () {
+
+return( 
+
+   <main className="container">
+    <p>Hello I'm</p>
+    <section className="animation">
+        <div className="first"><div> Back end developer</div></div>
+        <div className="second"> <div>Front end developer</div></div>
+        <div className="third"> <div>Full stack developer</div></div>
+    </section>
+
+   </main>
+)
+
+ }
+
+ export default Animation;
